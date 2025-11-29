0abb528172033ecc757961653450ac9ecb493ad3
AkashRathod2507 <akashrathod@orchidengg.ac.in>
2025-11-29 08:58:19 +0530

    Update CORS_ORIGIN to production URL

--- constants.js
+++ constants.js
@@ -1,3 +1,3 @@
 export const DB_NAME = "akashrathod";
 export const PORT = process.env.PORT || 8000;
-export const CORS_ORIGIN = process.env.CORS_ORIGIN || "http://localhost:5173";
+export const CORS_ORIGIN = https://saral-dqan.vercel.app

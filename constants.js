bc1b397da31067c9a2a483e0d88f48a1c97e607e
Akash Rathod <akashratho1110@gmail.com>
2025-11-29 09:44:28 +0530

    Fix merge conflict in constants.js

--- constants.js
+++ constants.js
@@ -1,9 +1,3 @@
-<<<<<<< HEAD
-export const DB_NAME = "akashrathod";
-export const PORT = process.env.PORT || 8000;
-export const CORS_ORIGIN =*
-=======
-export const DB_NAME = "akashrathod";
+﻿export const DB_NAME = "akashrathod";
 export const PORT = process.env.PORT || 8000;
 export const CORS_ORIGIN = process.env.CORS_ORIGIN || "*";
->>>>>>> 8aade02 (Simplify CORS to allow all origins)
